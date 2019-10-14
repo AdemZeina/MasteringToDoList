d068b7c4d0cde3c36706a2bbfefcfeb782696154
ademzeina <msz.wu@hotmail.com>
2019-10-14 21:53:20 +0300

    tests

---
+++ masteringToDolist.Application.Tests/Builders/ToDoListBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace masteringToDolist.Application.Tests.Builders
+{
+    public class ToDoListBuilder
+    {
+        public int ToDoListId1 => 123;
+        public int ToDoListId2 => 124;
+        public int ToDoListId3 => 125;
+        public string ToDoListName1 => "Development";
+        public string ToDoListName2 => "Testing";
+        public string ToDoListName3 => "Sport";
+    }
+}
---
+++ masteringToDolist.Application.Tests/Services/ToDolistTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using masteringToDoList.Application.Services;
+using masteringToDoList.Core.Interfaces;
+using masteringToDoList.Core.Repository;
+using Moq;
+using Xunit;
+
+namespace masteringToDolist.Application.Tests.Services
+{
+    public class ToDoListTests
+    {
+        private Mock<IToDoListRepository> _mockToDoListRepository;
+        private Mock<IAppLogger<ToDoListService>> _mockAppLogger;
+
+        public ToDoListTests(Mock<IToDoListRepository> mockToDoListRepository, Mock<IAppLogger<ToDoListService>> mockAppLogger)
+        {
+            _mockToDoListRepository = mockToDoListRepository;
+            _mockAppLogger = mockAppLogger;
+        }
+
+
+        [Fact]
+        public async Task Get_ToDo_List()
+        {
+            //Test code
+        }
+
+        [Fact]
+        public async Task Create_New_List()
+        {
+            //Test code
+        }
+    }
+}
